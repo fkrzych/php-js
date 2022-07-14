750a84580a3ecb292a742d8997f7f0fb8e161d87
Filip <filip.krzych@student.uj.edu.pl>
2022-07-14 09:26:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once __DIR__ . '/PhoneKeyboardConverter.php';
+
+    $phoneKeyboardConverter = new PhoneKeyboardConverter();
+
+    echo $phoneKeyboardConverter->convertToNumeric('Ela nie ma kota');
+
+    echo '</br>';
+
+    echo $phoneKeyboardConverter->convertToNumeric('jablecznik');
+
+    echo '</br>';
+
+    echo $phoneKeyboardConverter->convertToString('5,2,22,555,33,222,9999,66,444,55');
+
+    echo '</br>';
+
+    echo $phoneKeyboardConverter->convertToString('33,555,2,0,66,444,33,0,6,2,0,55,666,8,2');
+
+    echo '</br>';
+
+    echo $phoneKeyboardConverter->convertToString('7,444,33,7777');
+
+
